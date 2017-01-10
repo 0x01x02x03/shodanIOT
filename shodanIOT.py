fd1f233735681ec27e16f520677e973baaaf333c
Nathan H <thewoosterisroot@gmail.com>
2017-01-10 18:45:57 +0000

    renamed python file.

---
+++ shodanIOT.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# shodanIOT.py
+#
+# thewoosterisroot
+
+
+import shodan
+import sys
+
+
+# Configuration
+API_KEY = "YOUR_API_KEY"
+
+
+# Input validiation
+if len(sys.argv) == 1:
+    print ("Usage: {} <search query".format(sys.argv[0]))
+    sys.exit(1)
+    
+try:
+    # Setup api
+    api = shodan.Shodan(API_KEY)
+    
+    # Perform the search
+    query = ' '.join(sys.argv[1:])
+    result = api.search(query)
+    
+    # Loop through matches and print each IP
+    for service in result['matches']:
+        print(service['ip_str'])
+except Exception as e:
+    print ("Error: {}".format(e))
+    sys.exit(1)
